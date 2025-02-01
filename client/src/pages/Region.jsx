4c3b13941173286c016ea4a5fd925e2c1fb18c8e
Den <Den-email@example.com>
2025-02-02 00:56:19 +0700

    Regions update

--- client/src/pages/Region.jsx
+++ client/src/pages/Region.jsx
@@ -1,6 +1,12 @@
+
+import loginBackground from "../assets/images/loginBackground.png";
+
 const Regions = () => {
   return (
-    <div>Regions</div>
+    <div>
+      <img src={loginBackground} className="w-full h-screen" />
+    </div>
+    
   )
 }
 export default Regions
